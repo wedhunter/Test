89a9b3af836e10824b4d14fac8b2f0d8ba2b930c
wedhunter <myemail1577@mail.ru>
2023-03-06 10:26:28 +0300

    Test
    Test

---
+++ food-app-react/src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <div className="max-w-[1640px] mx-auto p-4">
+      <div className="max-h-[500px] relative">
+        {/* Overlay */}
+        <div className="absolute w-full h-full text-gray-200 max-h-[500px] bg-black/40 flex flex-col justify-center">
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            The <span className="text-orange-500">Best</span>
+          </h1>
+          <h1 className="px-4 text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold">
+            <span className="text-orange-500">Foods</span> Devlivered
+          </h1>
+        </div>
+        <img
+          className="w-full max-h-[500px] object-cover"
+          src="https://mondointernazionale.com/img/articoli/il-futuro-vegetariano-ha-il-sapore-della-carne/pexels-valeria-boltneva-1639565.jpg"
+          alt="/"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
